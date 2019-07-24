cdc914edecc0fa58a6609bbaab7688b41a957f51
mybigfriendjo <mybigfriendjo@users.noreply.github.com>
2019-07-24 03:45:50 +0200

    initial commit; nothing worth of commiting; still putting it up as a placeholder so i maybe don't forget to work on that thing

---
+++ LandGen/Settings.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandGen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Window mapWindow = new Map();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mapWindow.Show();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            mapWindow.Close();
+        }
+    }
+}
