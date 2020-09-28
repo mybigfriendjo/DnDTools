7046597c70f602c859f494a8219f8623deb44968
mybigfriendjo <mybigfriendjo@users.noreply.github.com>
2020-09-28 19:21:39 +0200

    initial

---
+++ roll/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace roll
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #if DEBUG
+            args = new[] {"1d20", "+", "3"};
+            #endif
+
+            string fullInput = string.Join(" ",args);
+
+            Console.WriteLine(fullInput);
+        }
+    }
+}
